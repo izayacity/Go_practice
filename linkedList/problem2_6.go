1da87cad8f375fa0ed53985021f4139e733785f2
Archer <archer@Archers-MacBook-Pro.local>
2017-05-02 00:01:49 -0700

    2.6 Palindrome: Implement a function to check if a linked list is a palindrome.

--- linkedList/problem2_6.go
+++ linkedList/problem2_6.go
@@ -3,3 +3,21 @@ package linkedList
 /*
 2.6 Palindrome: Implement a function to check if a linked list is a palindrome.
 */
+func (ll *DoubleLinkedList) IsPalindrome() bool {
+	if ll.head == nil || ll.head == ll.tail {
+		return true
+	}
+
+	node1 := ll.head
+	node2 := ll.tail
+
+	for node1 != node2 && node1.next != node2 {
+		if node1.value != node2.value {
+			return false
+		}
+
+		node1 = node1.next
+		node2 = node2.prev
+	}
+	return true
+}
